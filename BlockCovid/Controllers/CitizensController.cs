503a2e19e51aca7752d38343a52fb7173369025c
Barman <rayan.barman@hotmail.com>
2020-12-02 14:48:30 +0100

    bugfix appsetings.json

--- BlockCovid/Controllers/CitizensController.cs
+++ BlockCovid/Controllers/CitizensController.cs
@@ -43,8 +43,6 @@ namespace BlockCovid.Controllers
             return citizen;
         }
 
-        
-
         // POST: api/Citizens
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]

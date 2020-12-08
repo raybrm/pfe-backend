9e3ff8160b0daaa3d602e6145d702a9fbfd75a8f
GPO <alexlesgo@gmail.com>
2020-12-08 10:38:45 +0100

    remplacement de requete pour les notifs

--- BlockCovid/Dal/Repositories/EFQrCodesRepository.cs
+++ BlockCovid/Dal/Repositories/EFQrCodesRepository.cs
@@ -138,12 +138,12 @@ namespace BlockCovid.Dal.Repositories
             await Task.Run(async () =>
                await listCustomer.ForEachAsync(action: citizenQrCode =>
                 {
-
+                    DateTime dateMoins10Jours = citizenQrCode.Timestamp.AddHours(-240);
                     DateTime datePlusUneHeure = citizenQrCode.Timestamp.AddHours(1);
                     IQueryable<CitizenDto> listCitizenDtoToNotify = (from CitizenQrCode citizenQr in _context.CitizenQrCode.Include(ct => ct.Citizen)
 
                                                                     where citizenQr.QrCodeId == citizenQrCode.QrCodeId
-                                                                    && citizenQr.Timestamp <= datePlusUneHeure
+                                                                    && dateMoins10Jours < citizenQr.Timestamp && citizenQrCode.Timestamp<= citizenQr.Timestamp &&    citizenQr.Timestamp <= datePlusUneHeure
                                                                     && citizenQr.Citizen.Is_Positive==false
                                                                     select _mapper.Map<CitizenDto>(citizenQr.Citizen)).Distinct();
 
